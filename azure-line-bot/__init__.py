575f2f07b11131721019315aa7c778fe1d6f18b2
mochan_tk <takashi1.kawamoto@gmail.com>
2021-05-30 10:30:19 +0000

    commit

---
+++ azure-line-bot/__init__.py
@@ -0,0 +1,46 @@
+import logging
+import os
+import azure.functions as func
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+# Azure FunctionsのApplication Settingに設定した値から取得する↓
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # get x-line-signature header value
+    signature = req.headers['x-line-signature']
+
+    # get request body as text
+    body = req.get_body().decode("utf-8")
+    logging.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        func.HttpResponse(status_code=400)
+
+    return func.HttpResponse('OK')
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
